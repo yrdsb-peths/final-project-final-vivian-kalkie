d30191a7e008d5139fb0fd84f41e58c19809c370
vivianli19 <341207876@gapps.yrdsb.ca>
2025-05-20 14:47:31 -0400

    Update MyWorld.java

--- MyWorld.java
+++ MyWorld.java
@@ -10,8 +10,8 @@ public class MyWorld extends World {
     public void createBlock()
     {
         int numActors = 100;
-        int startingX = 1000;
-        int startingY = 50;
+        int startingX = 500;
+        int startingY = 100;
         int spacing = 20;
         
         for(int i = 0; i < numActors; i++)
