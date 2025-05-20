5d7b23e4d690ff8290ee170760b8fc3173bbc682
vivianli19 <341207876@gapps.yrdsb.ca>
2025-05-20 14:45:59 -0400

    fixing commits

--- MyWorld.java
+++ MyWorld.java
@@ -3,16 +3,8 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(1300, 600, 1);
-<<<<<<< HEAD
         createBlock();
-        
-=======
     
-        for(int i = 0; i < Greenfoot.getRandomNumber(15); i++)
-        {
-            createBlock();
-        }
->>>>>>> 799964846a27b0bdc930435eeb399610f7c92ea2
     }
     
     public void createBlock()
