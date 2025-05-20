12768217f7d6ff4e6495bf94e80c573552d2073b
Kalkie Eswarathasan <kalkiemithraa@gmail.com>
2025-05-20 14:44:17 -0400

    clean

--- MyWorld.java
+++ MyWorld.java
@@ -3,12 +3,10 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(1300, 600, 1);
-<<<<<<< HEAD
         
         // Create the scubadiver object 
         Scubadiver scubadiver = new Scubadiver();
         addObject(scubadiver, 300, 250);
-=======
     
         for(int i = 0; i < Greenfoot.getRandomNumber(15); i++)
         {
