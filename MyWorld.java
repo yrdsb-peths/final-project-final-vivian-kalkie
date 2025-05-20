0070227af110a863efa11e71ea3fa30a6099792b
vivianli19 <341207876@gapps.yrdsb.ca>
2025-05-20 14:17:33 -0400

    adding a block to the world

--- MyWorld.java
+++ MyWorld.java
@@ -4,4 +4,12 @@ public class MyWorld extends World {
     public MyWorld() {
         super(1300, 600, 1);
     }
+    
+    public void createBlock()
+    {
+        Block block = new Block();
+        int x = 1300;
+        int y = Greenfoot.getRandomNumber(600);
+        addObject(block, x, y);
+    }
 }
