12f9087f868d94fbb31b4cface193d5a5b060ce3
vivianli19 <341207876@gapps.yrdsb.ca>
2025-05-20 14:45:11 -0400

    making a moving wall

--- MyWorld.java
+++ MyWorld.java
@@ -3,18 +3,21 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(1300, 600, 1);
+        createBlock();
         
-        for(int i = 0; i < Greenfoot.getRandomNumber(15); i++)
-        {
-            createBlock();
-        }
     }
     
     public void createBlock()
     {
-        Block block = new Block();
-        int x = 1300;
-        int y = Greenfoot.getRandomNumber(600);
-        addObject(block, x, y);
+        int numActors = 100;
+        int startingX = 1000;
+        int startingY = 50;
+        int spacing = 20;
+        
+        for(int i = 0; i < numActors; i++)
+        {
+            Block block = new Block();
+            addObject(block, startingX + i*spacing, startingY);
+        }
     }
 }
