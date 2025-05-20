3950f6007d7ead2f5572b25d47e4b5bb95f7e312
Kalkie Eswarathasan <kalkiemithraa@gmail.com>
2025-05-20 14:31:09 -0400

    added scubadiver object

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,9 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(1300, 600, 1);
+        
+        // Create the scubadiver object 
+        Scubadiver scubadiver = new Scubadiver();
+        addObject(scubadiver, 300, 250);
     }
 }
