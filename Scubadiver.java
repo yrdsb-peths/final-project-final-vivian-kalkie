bd9677188c1159a9879d34d2f4eb2a385208c124
Kalkie Eswarathasan <kalkiemithraa@gmail.com>
2025-05-21 13:53:34 -0400

    ,

--- Scubadiver.java
+++ Scubadiver.java
@@ -2,17 +2,20 @@ import greenfoot.*;
 
 public class Scubadiver extends Actor
 {
-    //GreenfootSound bubblepopSound = new GreenfootSound("bubblepop.mp3");
+    GreenfootSound bubblepopSound = new GreenfootSound("bubblepop.mp3");
     
     public void act()
     {
         if(Greenfoot.isKeyDown("down"))
         {
             move(-1);
+            bubblepopSound.play();
         }
         else if(Greenfoot.isKeyDown("up"))
         {
             move(1);
+            bubblepopSound.play();
         }
     }
+    
 }
