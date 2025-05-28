af065f15fb1473d3e62e101ef722c8e0a91f607d
Kalkie Eswarathasan <kalkiemithraa@gmail.com>
2025-05-28 13:48:45 -0400

    creating powerup

---
+++ SpeedPowerUp.java
@@ -0,0 +1,14 @@
+import greenfoot.*; 
+/**
+ * A powerup that the player can run over and it will increase speed for a few seconds
+ * 
+ * @author Kalkie
+ * @version May 2025
+ */
+public class SpeedPowerUp extends Actor
+{
+    public SpeedPowerUp()
+    {
+        Greenfoot.setImage
+    }
+}
